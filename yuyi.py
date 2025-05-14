d6b73e36877c54d8509946ee03888c8d553cb386
th <2753521172@qq.com>
2025-05-14 20:56:30 +0800

    Merge remote-tracking branch 'origin/语法第二版修改' into 语法第二版修改
    # Conflicts:
    #	README.md
    #	cifa.py
    #	yufa.py

--- yuyi.py
+++ yuyi.py
@@ -1,8 +1,33 @@
 class Yuyi:
     def __init__(self):
         self.symbol_table = {}  # 符号表，存储变量名和类型
+        self.function_table = {}  # 函数表，存储函数名、参数类型和返回类型
         self.errors = []        # 语义错误列表
+        self.scopes = [{"global": {}}]  # 作用域栈，初始只有全局作用域
         self.current_scope = "global"  # 当前作用域
+
+    def enter_scope(self, scope_name):
+        """进入新的作用域"""
+        self.scopes.append({scope_name: {}})
+        self.current_scope = scope_name
+
+    def exit_scope(self):
+        """退出当前作用域"""
+        if len(self.scopes) > 1:  # 保持至少有一个全局作用域
+            self.scopes.pop()
+            self.current_scope = list(self.scopes[-1].keys())[0]
+
+    def add_symbol(self, name, type_info):
+        """添加符号到当前作用域"""
+        self.scopes[-1][self.current_scope][name] = type_info
+
+    def lookup_symbol(self, name):
+        """查找符号，从内层作用域向外层查找"""
+        for scope in reversed(self.scopes):
+            for scope_name, symbols in scope.items():
+                if name in symbols:
+                    return symbols[name]
+        return None
 
     def analyze(self, ast):
         """
@@ -15,7 +40,10 @@ class Yuyi:
             
         # 清空符号表和错误列表
         self.symbol_table = {}
+        self.function_table = {}
         self.errors = []
+        self.scopes = [{"global": {}}]
+        self.current_scope = "global"
         
         # 分析程序体
         for stmt in ast['body']:
@@ -24,6 +52,7 @@ class Yuyi:
         return {
             'type': 'semantic_analysis',
             'symbol_table': self.symbol_table,
+            'function_table': self.function_table,
             'errors': self.errors
         }
     
@@ -37,6 +66,12 @@ class Yuyi:
             self.analyze_if_statement(stmt)
         elif stmt['type'] == 'while_statement':
             self.analyze_while_statement(stmt)
+        elif stmt['type'] == 'function_declaration':
+            self.analyze_function_declaration(stmt)
+        elif stmt['type'] == 'function_call':
+            self.analyze_function_call(stmt)
+        elif stmt['type'] == 'return_statement':
+            self.analyze_return_statement(stmt)
     
     def analyze_variable_declaration(self, stmt):
         """分析变量声明"""
@@ -100,9 +135,104 @@ class Yuyi:
         for body_stmt in stmt['body']:
             self.analyze_statement(body_stmt)
     
+    def analyze_function_declaration(self, stmt):
+        """分析函数声明"""
+        func_name = stmt['name']
+        return_type = stmt['return_type']
+        params = stmt['params']
+
+        # 检查函数是否已声明
+        if func_name in self.function_table:
+            self.errors.append(f"第{stmt['line']}行：函数 '{func_name}' 重复声明")
+            return
+
+        # 创建函数信息
+        func_info = {
+            'return_type': return_type,
+            'params': [(param['name'], param['type']) for param in params],
+            'line': stmt['line']
+        }
+        self.function_table[func_name] = func_info
+
+        # 进入函数作用域
+        self.enter_scope(func_name)
+
+        # 添加参数到函数作用域
+        for param in params:
+            self.add_symbol(param['name'], param['type'])
+
+        # 分析函数体
+        for body_stmt in stmt['body']:
+            self.analyze_statement(body_stmt)
+
+        # 检查返回语句
+        if not self.has_return_statement(stmt['body']):
+            if return_type != 'void':
+                self.errors.append(f"第{stmt['line']}行：函数 '{func_name}' 缺少返回语句")
+
+        # 退出函数作用域
+        self.exit_scope()
+
+    def analyze_function_call(self, expr):
+        """分析函数调用"""
+        func_name = expr['name']
+        args = expr['arguments']
+
+        # 检查函数是否已声明
+        if func_name not in self.function_table:
+            self.errors.append(f"第{expr['line']}行：函数 '{func_name}' 未声明")
+            return None
+
+        func_info = self.function_table[func_name]
+        
+        # 检查参数数量
+        if len(args) != len(func_info['params']):
+            self.errors.append(f"第{expr['line']}行：函数 '{func_name}' 参数数量不匹配")
+            return None
+
+        # 检查参数类型
+        for i, (arg, (param_name, param_type)) in enumerate(zip(args, func_info['params'])):
+            arg_type = self.get_expression_type(arg)
+            if arg_type != param_type:
+                self.errors.append(f"第{expr['line']}行：函数 '{func_name}' 第{i+1}个参数类型不匹配，期望 {param_type}，得到 {arg_type}")
+
+        return func_info['return_type']
+
+    def has_return_statement(self, body):
+        """检查函数体是否包含返回语句"""
+        for stmt in body:
+            if stmt['type'] == 'return_statement':
+                return True
+            elif stmt['type'] in ['if_statement', 'while_statement']:
+                if self.has_return_statement(stmt['body']):
+                    return True
+                if stmt.get('else') and self.has_return_statement(stmt['else']):
+                    return True
+        return False
+
+    def analyze_return_statement(self, stmt):
+        """分析返回语句"""
+        if self.current_scope == "global":
+            self.errors.append(f"第{stmt['line']}行：返回语句不能在全局作用域")
+            return
+
+        func_info = self.function_table[self.current_scope]
+        if func_info['return_type'] == 'void':
+            if stmt.get('value'):
+                self.errors.append(f"第{stmt['line']}行：void函数不能返回值")
+        else:
+            if not stmt.get('value'):
+                self.errors.append(f"第{stmt['line']}行：非void函数必须返回值")
+            else:
+                return_type = self.get_expression_type(stmt['value'])
+                if return_type != func_info['return_type']:
+                    self.errors.append(f"第{stmt['line']}行：返回类型不匹配，期望 {func_info['return_type']}，得到 {return_type}")
+
     def get_expression_type(self, expr):
         """获取表达式的类型"""
-        if expr['type'] == 'number':
+        if expr['type'] == 'function_call':
+            return self.analyze_function_call(expr)
+        elif expr['type'] == 'number':
             # 根据数值判断类型
             value = expr['value']
             if '.' in value:
